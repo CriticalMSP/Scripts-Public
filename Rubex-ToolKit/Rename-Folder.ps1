0dac89f61e5466b9f824f29bd60ec98990aacbe7
criticalengineers <93341801+criticalengineers@users.noreply.github.com>
2024-07-03 16:43:04 -0400

    Update Rename-Folder.ps1
    Moved code inside

--- Rubex-ToolKit/Rename-Folder.ps1
+++ Rubex-ToolKit/Rename-Folder.ps1
@@ -1,1 +1,21 @@
-test
+# Script to rename the folder
+param (
+       $folderPath = "C:\Users\$env:username\AppData\Roaming\Rubex"
+    )
+
+    # Check if the folder exists
+    if (Test-Path $folderPath -PathType Container) {
+        # Get the current folder name
+        $folderName = (Get-Item $folderPath).Name
+        
+        # New folder name with .old appended
+        $date = Get-Date -Format "-MM-dd-yyyy"
+        $newFolderName = $folderName + $date + ".old" 
+        
+        # Rename the folder
+        Rename-Item -Path $folderPath -NewName $newFolderName
+        
+        Write-Output "Folder renamed from '$folderName' to '$newFolderName'."
+    } else {
+        Write-Output "Folder '$folderPath' does not exist."
+    }
